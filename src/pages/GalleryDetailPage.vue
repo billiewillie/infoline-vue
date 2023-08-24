6db6a055b221295310846abd8bc0f07c3d5b7e02
BiLLie WiLLie <vadimbelinovich@gmail.com>
2023-08-24 12:40:15 +0300

    added gallery download

--- src/pages/GalleryDetailPage.vue
+++ src/pages/GalleryDetailPage.vue
@@ -1,6 +1,33 @@
 <template>
   <div class="basepage gallery-page">
     <h1 class="title">{{ gallery.title }}</h1>
+    <div class="stats-wrapper">
+      <div class="stats rounded shadow">
+        <div class="stats__item">
+        <span class="icon">
+          <IconView/>
+        </span>
+          <span class="count">{{ gallery.show_count }}</span>
+        </div>
+        <div class="stats__item">
+        <span class="icon">
+          <IconPhoto/>
+        </span>
+          <span class="count">32</span>
+        </div>
+        <div class="stats__item">
+          <span class="icon">
+            <IconSave/>
+          </span>
+          <a
+              class="link"
+              :href="`https://gallery.trifonov.space/upload/galleries/${gallery.id}/${gallery.id}.zip`"
+              download>
+            скачать
+          </a>
+        </div>
+      </div>
+    </div>
     <div class="gallery-grid">
       <div
           class="gallery-item rounded shadow pic"
@@ -8,11 +35,7 @@
           :key="index"
           @click="() => showImg(index)">
         <div class="gallery-cover rounded overflow-hidden">
-          <TheImage
-              alt="photo"
-              :image="photo"
-          />
-
+          <TheImage alt="photo" :image="photo"/>
         </div>
       </div>
       <vue-easy-lightbox
@@ -31,6 +54,9 @@ import {useRootStore} from "@/stores/galleriesStore";
 import {storeToRefs} from "pinia";
 import {useRoute} from "vue-router";
 import TheImage from "@/components/TheImage.vue";
+import IconView from "@/components/icons/IconView.vue";
+import IconSave from "@/components/icons/IconSave.vue";
+import IconPhoto from "@/components/icons/IconPhoto.vue";
 
 const visibleRef = ref(false);
 const indexRef = ref(0);
@@ -171,5 +197,53 @@ const {gallery, galleryImgs} = storeToRefs(galleriesStore);
 
 .vel-modal {
   background: rgba(0, 0, 0, 0.8);
+}
+
+.stats-wrapper {
+  display: flex;
+  justify-content: center;
+}
+
+.stats {
+  display: flex;
+  align-items: center;
+  background-color: var(--white);
+  justify-content: center;
+  margin: 0 auto 30px;
+  padding: 20px;
+  -webkit-column-gap: 16px;
+  -moz-column-gap: 16px;
+  column-gap: 16px;
+}
+
+.stats__item {
+  display: flex;
+  align-items: center;
+  column-gap: 6px;
+}
+
+.stats__item a,
+.stats__item span {
+  color: var(--blue-dark);
+  line-height: 1;
+}
+
+.stats__item a.link {
+  text-decoration: underline;
+  text-underline-offset: 2px;
+}
+
+.stats__item a.link:hover {
+  text-decoration: none;
+}
+
+.stats__item .icon {
+  width: 16px;
+  height: 16px;
+}
+
+.stats__item .icon svg {
+  width: 100%;
+  height: 100%;
 }
 </style>
