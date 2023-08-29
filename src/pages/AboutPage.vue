075cebee9f658a2f6d3646873a42dccdad2fd757
BiLLie WiLLie <vadimbelinovich@gmail.com>
2023-08-29 15:30:37 +0300

    edited about page

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -56,35 +56,48 @@
               :slides-per-view="1"
               loop
               navigation>
-            <swiper-slide v-for="slide in slides1">
+            <swiper-slide v-for="slide in slides2">
               <TheImage alt="slide" :image="slide"/>
             </swiper-slide>
           </swiper>
           <div class="slider-text">
-            <h2 class="slider-title title">Мы проделали долгий путь</h2>
-            <p class="slider-description">Компания началась с нескольких человек
-              и небольшой комнаты в Больнице №33. За 30 лет мы выросли, сейчас нас больше
-              300 человек, мы занимаем целое здание, площадью
-              2500 м2, а наши офисы находятся не только
-              в Санкт-Петербурге, и даже не только в России.</p>
+            <h2 class="slider-title title">Команда профессионалов</h2>
+            <p class="slider-description">Многие наши сотрудники – профессионалы в своей области, получившие образование
+              в биологии
+              и медицине, и имеют опыт работы в лабораториях.
+              Мы регулярно проводим повышение квалификации, чтобы оставаться конкурентоспособными
+              на непростом рынке.</p>
           </div>
         </div>
       </div>
       <div class="timeline">
-        <div class="container">
-          <h2 class="title">История компании</h2>
-          <swiper
-              :slides-per-view="1"
-              :centered-slides="true"
-              space-between="30">
-            <swiper-slide
-                class="timeline-slide"
-                v-for="slide in timelineSlides">
-              <h2 class="year title">{{ slide.year }}</h2>
-              <p class="text">{{ slide.text }}</p>
-            </swiper-slide>
-          </swiper>
-        </div>
+        <h2 class="title">История компании</h2>
+        <swiper
+            :modules="[FreeMode]"
+            :freeMode="true"
+            :slides-per-view="'auto'"
+            :breakpoints="{
+                '768': {
+                  slidesPerView: 2
+                },
+                '1280': {
+                  slidesPerView: 3
+                },
+                '1900': {
+                  slidesPerView: 4
+                }
+              }"
+        >
+          <swiper-slide
+              class="timeline-slide"
+              v-for="slide in timelineSlides">
+            <div class="dot">
+              <span></span>
+            </div>
+            <h2 class="year title">{{ slide.year }}</h2>
+            <p class="text">{{ slide.text }}</p>
+          </swiper-slide>
+        </swiper>
       </div>
       <div class="about-page-title">
         <img class="bg-about-title" :src="BgAboutTitle" alt="bg-about-title">
@@ -102,17 +115,15 @@
               :slides-per-view="1"
               loop
               navigation>
-            <swiper-slide v-for="slide in slides1">
+            <swiper-slide v-for="slide in slides3">
               <TheImage alt="slide" :image="slide"/>
             </swiper-slide>
           </swiper>
           <div class="slider-text">
-            <h2 class="slider-title title">Мы проделали долгий путь</h2>
-            <p class="slider-description">Компания началась с нескольких человек
-              и небольшой комнаты в Больнице №33. За 30 лет мы выросли, сейчас нас больше
-              300 человек, мы занимаем целое здание, площадью
-              2500 м2, а наши офисы находятся не только
-              в Санкт-Петербурге, и даже не только в России.</p>
+            <h2 class="slider-title title">Имидж компании на высоте</h2>
+            <p class="slider-description">Ежегодно мы участвуем в главных выставочных проектах в сфере здравоохранения,
+              также в небольших региональных конференциях, проводим симпозиумы, мастер-классы и вебинары для
+              пользователей нашего оборудования.</p>
           </div>
         </div>
       </div>
@@ -125,26 +136,31 @@
             вместе с компанией на протяжении десятилетий.</p>
         </div>
       </div>
-      <div class="about-slider">
-        <div class="container">
-          <swiper
-              :modules="modules"
-              :slides-per-view="1"
-              loop
-              navigation>
-            <swiper-slide v-for="slide in slides1">
-              <TheImage alt="slide" :image="slide"/>
-            </swiper-slide>
-          </swiper>
-          <div class="slider-text">
-            <h2 class="slider-title title">Мы проделали долгий путь</h2>
-            <p class="slider-description">Компания началась с нескольких человек
-              и небольшой комнаты в Больнице №33. За 30 лет мы выросли, сейчас нас больше
-              300 человек, мы занимаем целое здание, площадью
-              2500 м2, а наши офисы находятся не только
-              в Санкт-Петербурге, и даже не только в России.</p>
-          </div>
-        </div>
+      <div class="about-slider about__success">
+        <h2 class="title">Истории успеха</h2>
+        <swiper
+            :modules="modules"
+            :slides-per-view="1"
+            loop
+            navigation>
+          <swiper-slide v-for="slide in slides4">
+            <TheImage alt="slide" :image="slide"/>
+            <div class="slider-text">
+              <h2 class="slider-title title">Первые полтора месяца были как в тумане...</h2>
+              <p class="slider-description">«Первые полтора месяца были, как в тумане - информации было просто море,
+                коллег еще больше, кому-то критически не понравилась доверенность за номером 69, что я помню
+                до сих пор. И самое яркое воспоминание - как постоянно звонил телефон, без остановки.
+                Я думала, что после аттестации будет проще, информации меньше и объемы работы перестанут так
+                восприниматься. Но нет - прошло почти шесть лет, а телефон все также без перерыва звонит. Работа по
+                подготовке доп. соглашений и доверенностей превратилась в…. теперь я могу заниматься вообще всем – и
+                представлением интересов компании в судах, ФАСах, госзаказом, таможенными спорами и чем угодно, что ты
+                еще придумаешь. В начале этого года я стала заместителем руководителя. И у меня такое ощущение, что я
+                так ничего и не узнала из того моря, которое может мне пригодиться».</p>
+              <p class="slider-name">Маргарита Данилова</p>
+              <p class="slider-position">Заместитель руководителя юридического отдела</p>
+            </div>
+          </swiper-slide>
+        </swiper>
       </div>
       <div class="about-page-title">
         <img class="bg-about-title" :src="BgAboutTitle" alt="bg-about-title">
@@ -162,17 +178,17 @@
               :slides-per-view="1"
               loop
               navigation>
-            <swiper-slide v-for="slide in slides1">
+            <swiper-slide v-for="slide in slides5">
               <TheImage alt="slide" :image="slide"/>
             </swiper-slide>
           </swiper>
           <div class="slider-text">
-            <h2 class="slider-title title">Мы проделали долгий путь</h2>
-            <p class="slider-description">Компания началась с нескольких человек
-              и небольшой комнаты в Больнице №33. За 30 лет мы выросли, сейчас нас больше
-              300 человек, мы занимаем целое здание, площадью
-              2500 м2, а наши офисы находятся не только
-              в Санкт-Петербурге, и даже не только в России.</p>
+            <h2 class="slider-title title">Отдыхаем со смыслом</h2>
+            <p class="slider-description">Вместе мы не только энергично работаем, но и круто отдыхаем несколько раз в
+              год. Ежегодно проводим для сотрудников 3-4 крупных мероприятия и несколько небольших, камерных, если
+              появляется повод для праздника. Традиционные события – осенний День здоровья, зимний Вечер подведения
+              итогов года, Детский новогодний утренник и Вечеринка в честь юбиляров – тех, кто работает с нами 5, 10, 15
+              и более лет.</p>
           </div>
         </div>
       </div>
@@ -183,7 +199,7 @@
 <script setup>
 import 'swiper/css';
 import 'swiper/css/navigation';
-import {Navigation} from 'swiper';
+import {Navigation, FreeMode} from 'swiper';
 import {Swiper, SwiperSlide} from 'swiper/vue';
 import TheImage from "@/components/TheImage.vue";
 import BgAboutTitle from "@/assets/img/about-title-bg.svg";
@@ -199,6 +215,25 @@ import ImageAboutPhoto14 from "@/assets/img/about-photo-14.webp"
 import ImageAboutPhoto15 from "@/assets/img/about-photo-15.webp"
 import ImageAboutPhoto16 from "@/assets/img/about-photo-16.webp"
 import ImageAboutPhoto17 from "@/assets/img/about-photo-17.webp"
+import ImagePro1 from "@/assets/img/pro-1.webp"
+import ImagePro2 from "@/assets/img/pro-2.webp"
+import ImagePro3 from "@/assets/img/pro-3.webp"
+import ImagePro4 from "@/assets/img/pro-4.webp"
+import ImagePro5 from "@/assets/img/pro-5.webp"
+import ImagePro6 from "@/assets/img/pro-6.webp"
+import ImageAboutImage1 from "@/assets/img/image-1.webp"
+import ImageAboutImage2 from "@/assets/img/image-2.webp"
+import ImageAboutImage3 from "@/assets/img/image-3.webp"
+import ImageAboutImage4 from "@/assets/img/image-4.webp"
+import ImageAboutImage5 from "@/assets/img/image-5.webp"
+import ImageAboutImage6 from "@/assets/img/image-6.webp"
+import ImageRelax1 from "@/assets/img/relax-1.webp"
+import ImageRelax2 from "@/assets/img/relax-2.webp"
+import ImageRelax3 from "@/assets/img/relax-3.webp"
+import ImageRelax5 from "@/assets/img/relax-5.webp"
+import ImageRelax6 from "@/assets/img/relax-6.webp"
+import ImageRelax7 from "@/assets/img/relax-7.webp"
+import AboutDanilova from "@/assets/img/about-danilova.webp";
 
 const slides1 = [
   ImageAboutPhoto11,
@@ -208,6 +243,38 @@ const slides1 = [
   ImageAboutPhoto15,
   ImageAboutPhoto16,
   ImageAboutPhoto17
+];
+
+const slides2 = [
+  ImagePro1,
+  ImagePro2,
+  ImagePro3,
+  ImagePro4,
+  ImagePro5,
+  ImagePro6
+];
+
+const slides3 = [
+  ImageAboutImage1,
+  ImageAboutImage2,
+  ImageAboutImage3,
+  ImageAboutImage4,
+  ImageAboutImage5,
+  ImageAboutImage6
+];
+
+const slides4 = [
+  AboutDanilova,
+  AboutDanilova
+];
+
+const slides5 = [
+  ImageRelax1,
+  ImageRelax2,
+  ImageRelax3,
+  ImageRelax5,
+  ImageRelax6,
+  ImageRelax7
 ];
 
 const timelineSlides = [
@@ -377,6 +444,100 @@ h1.title {
   padding: 0;
 }
 
+.about-slider.about__success {
+  width: 100%;
+  max-width: 1600px;
+  margin: auto;
+}
+
+.about-slider.about__success .image {
+  margin-bottom: 30px;
+
+  @media (min-width: 1280px) {
+    margin-bottom: 0;
+  }
+}
+
+.about-slider.about__success .swiper-slide {
+  padding: 0 10px;
+
+  @media (min-width: 1280px) {
+    display: flex;
+    flex-direction: row;
+    column-gap: 75px;
+  }
+
+  @media (min-width: 1920px) {
+    column-gap: 175px;
+    padding: 0 100px;
+  }
+}
+
+.about-slider.about__success .swiper-slide .slider-text::before,
+.about-slider.about__success .swiper-slide .slider-text::after {
+  content: '“';
+  position: absolute;
+  font-size: 144px;
+  color: var(--blue-light);
+  line-height: 1;
+}
+
+.about-slider.about__success .swiper-slide .slider-text::before {
+  top: -15px;
+  left: 0;
+}
+
+.about-slider.about__success .swiper-slide .slider-text::after {
+  transform-origin: 50%;
+  transform: rotate(180deg);
+  bottom: 180px;
+  right: 0;
+}
+
+.about-slider.about__success > .title {
+  text-align: center;
+  margin-bottom: 25px;
+
+  @media (min-width: 1280px) {
+    margin-bottom: 80px;
+    font-size: 30px;
+  }
+}
+
+.about-slider.about__success .swiper-slide .slider-text {
+  position: relative;
+
+  @media (min-width: 1920px) {
+    padding: 60px 60px 0 0;
+  }
+}
+
+@media (min-width: 1280px) {
+  .about-slider.about__success .swiper-slide .slider-text::before,
+  .about-slider.about__success .swiper-slide .slider-text::after {
+    display: block;
+  }
+}
+
+@media (min-width: 1920px) {
+  .about-slider.about__success .swiper-slide .slider-text::before {
+    left: -50px;
+  }
+
+  .about-slider.about__success .swiper-slide .slider-text::after {
+    right:  10px;
+    bottom: 240px;
+  }
+}
+
+.about-slider.about__success .swiper-slide .slider-name {
+  font-weight: 700;
+  font-size: 17px;
+  padding-bottom: 20px;
+  padding-top: 20px;
+  border-bottom: 1px solid var(--gray-medium);
+}
+
 .about-logos {
   display: flex;
   flex-direction: column;
@@ -510,29 +671,76 @@ h1.title {
 }
 
 .timeline {
+  display: flex;
+  flex-direction: column;
   text-align: center;
-}
-
-.timeline .container {
   row-gap: 30px;
+  width: 100%;
+  max-width: 1400px;
+  margin: auto;
 
   @media (min-width: 1280px) {
     row-gap: 70px;
   }
 }
+
+.timeline .swiper {
+  width: 100%;
+}
+
 .timeline .title {
   font-size: 30px;
 }
 
 .timeline-slide {
+  position: relative;
   display: flex;
   flex-direction: column;
   align-items: flex-start;
   row-gap: 20px;
   text-align: left;
-  padding: 20px 0;
+  padding: 25px 10px 0;
   z-index: 10;
-  border-top: 2px solid var(--gray-medium);
+  background-image: url('@/assets/img/timeline.webp');
+  background-repeat: repeat-x;
+  background-position: 0 2px;
+  cursor: grab;
+
+  @media (min-width: 1280px) {
+    padding: 25px 70px 0 10px;
+  }
+}
+
+.timeline-slide .dot {
+  position: absolute;
+  background-color: var(--white);
+  -webkit-border-radius: 50%;
+  -moz-border-radius: 50%;
+  border-radius: 50%;
+  width: 22px;
+  height: 22px;
+  top: 0;
+  left: 0;
+}
+
+.timeline-slide .dot span {
+  position: absolute;
+  width: 16px;
+  height: 16px;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+  background-color: var(--white);
+  border: 5px solid var(--blue-light);
+  -webkit-border-radius: 50%;
+  -moz-border-radius: 50%;
+  border-radius: 50%;
+}
+
+.timeline-slide:last-child {
+  background-image: none;
 }
 
 .timeline-slide .year {
