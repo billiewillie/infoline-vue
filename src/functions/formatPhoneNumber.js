518bfe44b94aebda95a62edf03056f99afd5c881
BiLLie WiLLie <vadimbelinovich@gmail.com>
2023-09-19 09:00:42 +0300

    update

--- src/functions/formatPhoneNumber.js
+++ src/functions/formatPhoneNumber.js
@@ -1,6 +1,6 @@
 export function formatPhoneNumber(phone, country = 'russia') {
     if (phone) {
-        if (country === 'belorussia') {
+        if (country === 'belorussia' || country === 'tashkent') {
             return phone.replace(/(\d{3})(\d{2})(\d{3})(\d{2})(\d{2})/, '+$1 ($2) $3-$4-$5');
         }
         return phone.replace(/(\d)(\d{3})(\d{3})(\d{2})(\d{2})/, '+$1 ($2) $3-$4-$5');
