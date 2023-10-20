c6e0376f28f0ce8d487976e8a71bbd29650f4329
BiLLie WiLLie <vadimbelinovich@gmail.com>
2023-10-20 17:36:37 +0300

    edited contest

--- src/stores/contestStore.js
+++ src/stores/contestStore.js
@@ -49,7 +49,8 @@ export const useRootStore = defineStore(
 
         const addImageToSelectedImages = (item, category, user_id) => {
             if (selectedImages.value.find(el => el.category === category).data.length < contestData.value.list.find(el => el.category === category).limit
-                && !selectedImages.value.find(el => el.category === category).data.includes(item.id)) {
+                && !selectedImages.value.find(el => el.category === category).data.includes(item.id)
+                && user_id !== item.author_id) {
                 selectedImages.value.find(el => el.category === category).data.push(item.id);
                 console.log(item.author_id);
                 console.log(user_id);
