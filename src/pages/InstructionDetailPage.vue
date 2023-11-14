d1a479a0c85e8597c761529a17f74eeec911a36e
BiLLie WiLLie <vadimbelinovich@gmail.com>
2023-11-14 12:44:20 +0300

    edited instuction

--- src/pages/InstructionDetailPage.vue
+++ src/pages/InstructionDetailPage.vue
@@ -25,12 +25,12 @@
             </span>
           </a>
         </div>
+        <p v-else>Инструкция скоро будет</p>
         <div v-if="instruction.media?.video[0]?.url" class="instruction-video">
           <video class="video" controls>
             <source :src="`${instruction.media?.video[0]?.url}`">
           </video>
         </div>
-        <p v-else>Инструкция скоро будет</p>
       </div>
     </div>
   </div>
